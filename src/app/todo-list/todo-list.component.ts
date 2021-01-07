2f3e5925392beb59d1eaac7fac1bcb986965f99b
Francisco Marinho <jfmn@cesar.org.br>
2021-01-07 10:55:39 -0300

    adjustment in to sample

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,18 +11,12 @@ export class TodoListComponent implements OnInit, OnDestroy {
 
   todoList = [];
 
-  brodaCastChannel: BroadcastChannel;
-  readonly CHANNEL = 'todoChannel'
-
-  constructor(private changeDetector: ChangeDetectorRef) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
   ngOnDestroy(): void {
-    if (this.brodaCastChannel) {
-      this.brodaCastChannel.close();
-    }
   }
 
   addItem() {
